536adc40066870de8e868f20c06aede7dcf21e02
Lars Jeppesen <larsjep@lego.fake>
2013-10-05 23:24:00 +0200

    Completed button support

--- LcdExample/Program.cs
+++ LcdExample/Program.cs
@@ -2,11 +2,13 @@ using System;
 using MonoBrickFirmware.IO;
 using MonoBrickFirmware.Graphics;
 using System.Resources;
+using System.Threading;
 
 namespace example
 {
 	class MainClass
 	{
+		static EventWaitHandle stopped = new ManualResetEvent(false);
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
@@ -19,8 +21,21 @@ namespace example
 			Rect box = new Rect(p, p+boxSize);
 			lcd.WriteTextBox(f, box + offset*0, "Hello World!!", true);
 			lcd.WriteTextBox(f, box + offset*1, "Hello World!!", false);
-			lcd.WriteTextBox(f, box + offset*2, "Hello World!!", true);
+			lcd.WriteTextBox(f, box + offset*2, "Hello World!!", true);						
 			lcd.Update();
+			
+			Buttons buts = new Buttons();
+			int val = 7;
+			buts.EnterPressed += () =>
+			{ 
+				lcd.WriteTextBox(f, box, "Value = " + val.ToString(), true);
+				lcd.Update (); 				
+				val++;
+			};
+			buts.EscapePressed += () => stopped.Set();
+			buts.LeftPressed += () => stopped.Set();
+			
+			stopped.WaitOne();
 		}
 	}
 }
--- MonoBrickFirmware/IO/Buttons.cs
+++ MonoBrickFirmware/IO/Buttons.cs
@@ -6,24 +6,89 @@ namespace MonoBrickFirmware.IO
 {
 	public class Buttons : IDisposable
 	{
+		[Flags]
+		public enum ButtonStates
+		{
+			None		= 0x00,
+			Up			= 0x01,
+			Enter		= 0x02,
+			Down		= 0x04,
+			Right		= 0x08,
+			Left		= 0x10,
+			Escape		= 0x20,
+			All			= 0xff,
+		}		
+		public const int ButtonCount = 6;
+		public event Action UpPressed = delegate {};
+		public event Action EnterPressed = delegate {};
+		public event Action DownPressed = delegate {};
+		public event Action RightPressed = delegate {};
+		public event Action LeftPressed = delegate {};
+		public event Action EscapePressed = delegate {};
+		
 		UnixDevice dev;
 		MemoryArea buttonMem;
-		public const int ButtonCount = 6;
 		EventWaitHandle stopPolling = new ManualResetEvent(false);
 		const int pollTime = 50;
+		const int debounceTime = 10;
 		public Buttons ()
 		{
-			dev = new UnixDevice("/dev/lmd_ui");
+			dev = new UnixDevice("/dev/lms_ui");
 			buttonMem = dev.MMap(ButtonCount, 0);
 			new Thread(ButtonPollThread).Start();
 		}
 				
-		void ButtonPollThread()
+		ButtonStates ReadButtons()
 		{
+			ButtonStates bs = ButtonStates.None;
+			byte[] buttonData = buttonMem.Read(0, ButtonCount);
+			int bitMask = 1;
+			foreach (byte butState in buttonData)
+			{
+				if (butState != 0)
+					bs |= (ButtonStates)bitMask;
+				bitMask = bitMask << 1;
+			}
+			return bs;
+		}
+		
+		ButtonStates GetDebounced()
+		{
+			
+			ButtonStates s2 = ButtonStates.None;
+			for (;;)
+			{
+				ButtonStates s1 = ReadButtons();
+				if (s1 == s2)
+					return s1;
+				Thread.Sleep(debounceTime);
+				s2 = ReadButtons();
+			}
+				
+		}
+		
+		void ButtonPollThread()
+		{	
+			Thread.CurrentThread.IsBackground = true;
+			ButtonStates lastState = GetDebounced();
 			while (!stopPolling.WaitOne(pollTime))
 			{
-				byte[] buttonData = new byte[6];
-				//buttonMem.
+				ButtonStates bs = GetDebounced();
+				if (bs != lastState)
+				{
+					ButtonStates pressed = (bs ^ lastState) & (~lastState);
+					switch (pressed)
+					{
+						case ButtonStates.Down:		DownPressed(); break;
+						case ButtonStates.Enter:	EnterPressed(); break;
+						case ButtonStates.Escape:	EscapePressed(); break;
+						case ButtonStates.Left:		LeftPressed(); break;
+						case ButtonStates.Right:	RightPressed(); break;
+						case ButtonStates.Up:		UpPressed(); break;
+					}
+					lastState = bs;
+				}
+				
 			}
 		}
 
