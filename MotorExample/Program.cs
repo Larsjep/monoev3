b44a0d636c8bb89c81daa532c43b5c0ab9d442d9
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2013-10-06 00:47:29 +0200

    Added new motor test program

--- MotorExample/Program.cs
+++ MotorExample/Program.cs
@@ -6,7 +6,7 @@ namespace MotorExample
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Running motor test");
+			/*Console.WriteLine ("Running motor test");
 			Motor motorA = new Motor(MotorPort.OutA);
 			Console.WriteLine ("Reset motor tacho");
 			motorA.ResetTacho();
@@ -29,8 +29,43 @@ namespace MotorExample
 			Console.WriteLine ("Move to zero");
 			Console.WriteLine (motorA.GetTachoCount());
 			motorA.MoveTo(10, 0, true);
-			Console.WriteLine ("done executing");
+			Console.WriteLine ("done executing");*/
 			
+			Console.WriteLine ("Running motor test");
+			Motor motorA = new Motor (MotorPort.OutA);
+			Motor motorB = new Motor (MotorPort.OutB);
+			Motor motorC = new Motor (MotorPort.OutC);
+			Motor motorD = new Motor(MotorPort.OutD);
+			motorA.ResetTacho();
+			motorB.ResetTacho();
+			motorC.ResetTacho();
+			motorD.ResetTacho();
+			motorA.Off();
+			motorB.Off();
+			motorC.Off();
+			motorD.Off();
+			motorA.GetTachoCount();
+			Motor motorToMove = motorA;
+			for(int i = 0; i < 5; i++){
+				System.Threading.Thread.Sleep(5000);
+				Console.WriteLine("");
+				Console.WriteLine("***** new moving timer ******");
+				motorToMove.On(-20);
+				Console.WriteLine("Count:" + motorToMove.GetTachoCount());
+				Console.WriteLine("Speed:" + motorToMove.GetSpeed());
+						
+			}
+			motorToMove.Brake();
+			motorToMove.Off();
+			Console.WriteLine ("Done moving");
+			for(int i = 0; i < 5; i++){
+				System.Threading.Thread.Sleep(5000);
+				Console.WriteLine("");
+				Console.WriteLine("***** new stop timer ******");
+				Console.WriteLine("Count:" + motorToMove.GetTachoCount());
+			}
+			Console.WriteLine ("Done");
+			Console.WriteLine ("Now I am done");
 			
 		}
 	}
