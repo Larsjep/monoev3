bb8697b4ea18c63ac78cf79cd11c02148547e4db
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2013-11-13 22:55:52 +0100

    Updated sensor example to use ManualResetEvent

--- ColorSensorExample/Program.cs
+++ ColorSensorExample/Program.cs
@@ -2,41 +2,34 @@ using System;
 using MonoBrickFirmware;
 using MonoBrickFirmware.IO;
 using MonoBrickFirmware.Graphics;
+using System.Threading;
 namespace NXTColorSensorExample
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			ColorMode[] modes = {ColorMode.Color, ColorMode.Reflection, ColorMode.Ambient, ColorMode.Blue, ColorMode.Green};
+			EventWaitHandle stopped = new ManualResetEvent(false);
+			ColorMode[] modes = {ColorMode.Color, ColorMode.Reflection, ColorMode.Ambient, ColorMode.Blue};
 			int modeIdx = 0;
-			bool run = true;
 			var colorSensor = new ColorSensor(SensorPort.In1);
 			ButtonEvents buts = new ButtonEvents ();
 			
-			buts.EnterPressed += () => { 
-				run  = false;
+			buts.EscapePressed += () => { 
+				stopped.Set();
 			};
 			buts.UpPressed += () => { 
 				LcdConsole.WriteLine("Sensor value: " + colorSensor.ReadAsString());
 			};
-			buts.LeftPressed += () => { 
+			buts.DownPressed += () => { 
 				LcdConsole.WriteLine("Raw sensor value: " + colorSensor.ReadRaw());
 			};
-			buts.RightPressed += () => { 
-				RGBColor RGB = colorSensor.ReadRGBColor();
-				LcdConsole.WriteLine("Red value  : " + RGB.Red);
-				LcdConsole.WriteLine("Green value: " + RGB.Green);
-				LcdConsole.WriteLine("Blue value : " + RGB.Blue);
-			};
-			buts.DownPressed += () => { 
+			buts.EnterPressed += () => { 
 				modeIdx = (modeIdx+1)%modes.Length;
 				colorSensor.Mode = modes[modeIdx];
 				LcdConsole.WriteLine("Sensor mode is set to: " + modes[modeIdx]);
 			};  
-			while (run) {
-				System.Threading.Thread.Sleep(50);
-			}
+			stopped.WaitOne();
 		}
 	}
 }
