06877088e9eed351a0f4844594d8e68ef9a60506
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2013-11-14 00:34:51 +0100

    Updated motor example to use LcdConsole

--- ColorSensorExample/Program.cs
+++ ColorSensorExample/Program.cs
@@ -5,31 +5,31 @@ using MonoBrickFirmware.Graphics;
 using System.Threading;
 namespace NXTColorSensorExample
 {
-	class MainClass
+class MainClass
+{
+	public static void Main (string[] args)
 	{
-		public static void Main (string[] args)
-		{
-			EventWaitHandle stopped = new ManualResetEvent(false);
-			ColorMode[] modes = {ColorMode.Color, ColorMode.Reflection, ColorMode.Ambient, ColorMode.Blue};
-			int modeIdx = 0;
-			var colorSensor = new ColorSensor(SensorPort.In1);
-			ButtonEvents buts = new ButtonEvents ();
-			
-			buts.EscapePressed += () => { 
-				stopped.Set();
-			};
-			buts.UpPressed += () => { 
-				LcdConsole.WriteLine("Sensor value: " + colorSensor.ReadAsString());
-			};
-			buts.DownPressed += () => { 
-				LcdConsole.WriteLine("Raw sensor value: " + colorSensor.ReadRaw());
-			};
-			buts.EnterPressed += () => { 
-				modeIdx = (modeIdx+1)%modes.Length;
-				colorSensor.Mode = modes[modeIdx];
-				LcdConsole.WriteLine("Sensor mode is set to: " + modes[modeIdx]);
-			};  
-			stopped.WaitOne();
-		}
+		EventWaitHandle stopped = new ManualResetEvent(false);
+		ColorMode[] modes = {ColorMode.Color, ColorMode.Reflection, ColorMode.Ambient, ColorMode.Blue};
+		int modeIdx = 0;
+		var sensor = new ColorSensor(SensorPort.In1);
+		ButtonEvents buts = new ButtonEvents ();
+		
+		buts.EscapePressed += () => { 
+			stopped.Set();
+		};
+		buts.UpPressed += () => { 
+			LcdConsole.WriteLine("Sensor value: " + sensor.ReadAsString());
+		};
+		buts.DownPressed += () => { 
+			LcdConsole.WriteLine("Raw sensor value: " + sensor.ReadRaw());
+		};
+		buts.EnterPressed += () => { 
+			modeIdx = (modeIdx+1)%modes.Length;
+			sensor.Mode = modes[modeIdx];
+			LcdConsole.WriteLine("Sensor mode is set to: " + modes[modeIdx]);
+		};  
+		stopped.WaitOne();
 	}
+}
 }
