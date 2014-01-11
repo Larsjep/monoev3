ad8d9a1c151e514eb303b460a9dd3b9d9d5cffd1
Anders Søborg <anders@soborg.net>
2014-01-12 00:40:43 +0100

    Added a webserver example

---
+++ WebServerExample/MainWebForm.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using MonoBrickFirmware.Sensors;
+namespace WebServerExample
+{
+  public partial class MainWebForm : System.Web.UI.Page
+  {
+    //private static EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(SensorPort.In1);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    
+    }
+
+    protected void SensorTimer_Tick(object sender, EventArgs e)
+    {
+      //Sensor1ValueTextBox.Text = "Count: " + sensor.ReadAsString();
+    }
+
+
+
+
+  }
+}
---
+++ WebServerExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebServerExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebServerExample")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c02619e-a5ca-41a8-94a5-c0f8207e8ef9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
