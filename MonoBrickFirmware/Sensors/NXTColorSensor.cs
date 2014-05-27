7341e12bd1299bffcf46ea298ccc9fac01988d71
Christian <cn00@gmx.at>
2014-05-27 20:44:22 +0200

    NXTColorSensor: Fix constructor ignoring mode.
    The constructor set the mode from the AnalogMode property, effectively resulting in a self-assignment.

--- MonoBrickFirmware/Sensors/NXTColorSensor.cs
+++ MonoBrickFirmware/Sensors/NXTColorSensor.cs
@@ -131,7 +131,7 @@ namespace MonoBrickFirmware.Sensors
 		public NXTColorSensor (SensorPort port, ColorMode mode) :  base(port)
 		{
 			GetColorData();
-			SetMode(AnalogMode);	
+			SetMode((AnalogMode)mode);	
 		}
 		
 		/// <summary>
