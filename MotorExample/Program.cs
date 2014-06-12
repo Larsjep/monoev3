b51a75c246845e403380a7d134455053a6116307
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2014-06-12 21:16:28 +0200

    Updated Motor example

--- MotorExample/Program.cs
+++ MotorExample/Program.cs
@@ -11,52 +11,17 @@ namespace MotorExample
 		public static void Main (string[] args)
 		{
 			
+			
 			Motor motor = new Motor (MotorPort.OutA);
-			MotorSync motorSync = new MotorSync(MotorPort.OutA, MotorPort.OutD);
-			LcdConsole.WriteLine("Use Motor on A");
-			LcdConsole.WriteLine("Use Motor on D");
-			LcdConsole.WriteLine("Press Ent. to start");
-			Buttons.Instance.GetKeypress();
-			motor.ResetTacho ();
-			LcdConsole.WriteLine ("Running forward with 20");
-			motor.On(20);
-			System.Threading.Thread.Sleep(2500);
-			LcdConsole.WriteLine ("Printing motor speed: " + motor.GetSpeed());
-			System.Threading.Thread.Sleep(2500);
-			LcdConsole.WriteLine ("Running backwards with 70");
-			motor.On(-70);
+			motor.ResetTacho();
+			motor.MoveTo(25,1000,true,true);
 			System.Threading.Thread.Sleep(3000);
-			LcdConsole.WriteLine ("Reverse direction");
-			motor.Reverse = true;
+			Console.WriteLine(motor.GetTachoCount().ToString());
+			
+			/*motor.MoveTo(75,0,false,true);
+			LcdConsole.WriteLine(motor.GetTachoCount().ToString());
 			System.Threading.Thread.Sleep(3000);
-			LcdConsole.WriteLine ("Brake");
-			motor.Reverse = false;
-			motor.Brake();
-			System.Threading.Thread.Sleep(3000);			
-			LcdConsole.WriteLine ("Off");
-			motor.Off();
-			System.Threading.Thread.Sleep(3000);			
-			
-			LcdConsole.WriteLine ("Move to zero");
-			motor.MoveTo(40, 0, true);
-			LcdConsole.WriteLine("Motor at position: " + motor.GetTachoCount());
-			System.Threading.Thread.Sleep(2000);
-			
-			LcdConsole.WriteLine ("Creating a step profile");
-			motor.SpeedProfileStep(40,100, 1500, 100, true);
-			motor.Off();
-			LcdConsole.WriteLine("Motor at position: " + motor.GetTachoCount());
-			System.Threading.Thread.Sleep(2000);
-			
-			LcdConsole.WriteLine ("Motor " + motorSync.BitField + " synchronised forward for 2500 steps");
-			motorSync.On(50, 0, 2500, true);
-			motorSync.Off();
-			LcdConsole.WriteLine ("Motor " + motorSync.BitField + " synchronised with second motor moving at half speed");
-			motorSync.On(-20,50,2500, false); //coast when done
-			
-			
-			
-			LcdConsole.WriteLine ("Done executing motor test");
+			LcdConsole.WriteLine ("Done executing motor test");*/
 		}
 	}
 }
