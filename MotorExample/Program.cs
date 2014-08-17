c333a04b762513572caccd782f0c46ff75638d87
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2014-08-17 20:56:10 +0200

    Updated the motor example program

--- MotorExample/Program.cs
+++ MotorExample/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using MonoBrickFirmware;
 using MonoBrickFirmware.Movement;
 using MonoBrickFirmware.Display;
@@ -13,26 +14,49 @@ namespace MotorExample
 			
 			Motor motorA = new Motor (MotorPort.OutA);
 			Motor motorD = new Motor (MotorPort.OutD);
+			
+			motorA.Off();
+			motorD.Off();
 
-			/*Console.WriteLine("Current" + MonoBrickFirmware.Tools.VersionHelper.CurrentImageVersion());
-			MonoBrickFirmware.Tools.VersionInfo info = MonoBrickFirmware.Tools.VersionHelper.AvalibleVersions();
-			Console.WriteLine("Firmware: " +  info.Fimrware);
-			Console.WriteLine("Image: " +  info.Image);
-			Console.WriteLine("Addin: " +  info.AddIn);*/
+			motorA.ResetTacho();
+			motorD.ResetTacho();
+
+			LcdConsole.WriteLine("Set speed to 50");
+			motorA.SetSpeed(50);
+			Thread.Sleep(1000);
+			LcdConsole.WriteLine("Speed: " + motorA.GetSpeed());
+			Thread.Sleep(2000);
+			LcdConsole.WriteLine("Break");
+			motorA.Brake();
+
+			Thread.Sleep(3000);
+			motorA.ResetTacho();
+			LcdConsole.WriteLine("Moving motor A to 2000 ");
+			var motorWaitHandle =  motorA.SpeedProfile(50, 200, 1600, 200,true);
+			LcdConsole.WriteLine("Waiting for motor A to stop");
+			//you could do something else here
+			motorWaitHandle.WaitOne();
+			LcdConsole.WriteLine("Done moving motor");
+			LcdConsole.WriteLine("Position A: " + motorA.GetTachoCount());
 
 
-			/*motorA.ResetTacho();
-			motorD.ResetTacho ();
-			Console.WriteLine("Motor A: " + motorA.GetTachoCount().ToString());
-			Console.WriteLine("Motor D: " + motorD.GetTachoCount().ToString());
-
-			motorA.MoveTo(25,1000,true,false);
-			motorD.MoveTo(25,1000,true,true);
-
-			Console.WriteLine("Motor A: " + motorA.GetTachoCount().ToString());
-			System.Threading.Thread.Sleep (1000);
-			Console.WriteLine("Motor D: " + motorD.GetTachoCount().ToString());*/
-
+			Thread.Sleep(3000);
+			motorA.ResetTacho();
+			motorD.ResetTacho();
+			LcdConsole.WriteLine("Moving motors A to 2000");
+			LcdConsole.WriteLine("Moving motor B to 1000");
+			WaitHandle[] handles = new WaitHandle[2];
+			handles[0] =  motorA.SpeedProfile(50, 200, 1600, 200,true);
+			handles[1] = motorD.SpeedProfile(50,200,600,200,true);
+			LcdConsole.WriteLine("Waiting for both motors to stop");
+			//you could do something else here
+			WaitHandle.WaitAll(handles);
+			LcdConsole.WriteLine("Done moving both motors");
+			LcdConsole.WriteLine("Position A: " + motorA.GetTachoCount());
+			LcdConsole.WriteLine("Position D: " + motorD.GetTachoCount());
+			motorA.Off();
+			motorD.Off();
+			
 		}
 	}
 }
