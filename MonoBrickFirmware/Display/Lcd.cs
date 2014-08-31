3984292938bf3f8e299811823c93d1ace74047c7
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2014-08-31 23:21:26 +0200

    Added support for drawing a line

--- MonoBrickFirmware/Display/Lcd.cs
+++ MonoBrickFirmware/Display/Lcd.cs
@@ -177,6 +177,39 @@ namespace MonoBrickFirmware.Display
 		{
 			DrawBitmap(bm.GetStream(), p, bm.Width, bm.Height, true);
 		}
+
+		public void DrawLine (Point start, Point end, bool color)
+		{
+			int height = Math.Abs (end.Y - start.Y);
+			int width = Math.Abs (end.X - start.X);
+
+			int ix = start.X;
+			int iy = start.Y;
+			int sx = start.X < end.X ? 1 : -1;
+			int sy = start.Y < end.Y ? 1 : -1;
+
+			int err = width + (-height);
+			int e2;
+
+			do {
+				SetPixel (ix, iy, color);
+
+				if (ix == end.X && iy == end.Y)
+					break;
+
+				e2 = 2 * err;
+				if (e2 > (-height)) {
+					err += (-height);
+					ix += sx;
+				}
+				if (e2 < width) {
+					err += width;
+					iy += sy;
+				}
+
+			} while (true);
+
+		}
 		
 		public void DrawBitmap(BitStreamer bs, Point p, uint xsize, uint ysize, bool color)
 		{
