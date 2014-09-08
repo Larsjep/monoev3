2356ad5bd7a03c1e6e5eb4c5b42cbf6ac4b1f1f8
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2014-09-08 21:02:58 +0200

    Lcd extensions DrawCircle, DrawCircleFilled, DrawEllipse, DrawEllipseFilled, DrawRectangle are added;
    DrawLine is extracted to the individual Lcd extension class;
    LcdDrawExample is added.

---
+++ LcdDrawExample/ExamplesDialog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using MonoBrickFirmware.Display;
+using MonoBrickFirmware.Display.Dialogs;
+
+namespace LcdDraw
+{
+	public class ExamplesDialog: Dialog{
+
+		private int _currentScreen;
+		private Point _center;
+
+		public ExamplesDialog(string title) : 
+			base(Font.MediumFont, title, Lcd.Width, Lcd.Height-(int)Font.MediumFont.maxHeight)
+		{
+			_currentScreen = 1;
+			_center = new Point (Lcd.Width / 2, Lcd.Height / 2);
+		}
+
+		protected override bool OnEnterAction ()
+		{
+			_currentScreen = (_currentScreen == 1 ? 2 : 1);
+			Lcd.Instance.Clear ();
+
+			return false; // no exit
+		}
+
+		protected override bool OnEscape()
+		{
+			return true; // exit
+		}
+
+		protected override void OnDrawContent ()
+		{
+			if (_currentScreen == 1)
+				DrawFirstScreen ();
+			else
+				DrawSecondScreen ();
+		}
+
+		private void DrawFirstScreen()
+		{
+			Lcd.Instance.DrawLine( new Point (0, 0), new Point (Lcd.Width-1, Lcd.Height-1), true);
+			Lcd.Instance.DrawLine( new Point (0, Lcd.Height-1), new Point (Lcd.Width-1, 0), true);
+			Lcd.Instance.DrawLine( new Point (40, 40), new Point (Lcd.Width-40, Lcd.Height-40), true);
+			Lcd.Instance.DrawLine( new Point (40, 100), new Point (Lcd.Width-20, Lcd.Height-60), true);
+
+			Lcd.Instance.DrawCircle (_center, 60, true);
+			Lcd.Instance.DrawCircle (_center, 50, true);
+			Lcd.Instance.DrawCircle (_center, 40, true);
+			Lcd.Instance.DrawCircleFilled (_center, 30, true);
+			Lcd.Instance.DrawCircleFilled (_center, 20, false);
+			Lcd.Instance.DrawCircleFilled (_center, 10, true);
+
+			Lcd.Instance.DrawEllipse (_center, 60, 30, true);
+		}
+
+		private void DrawSecondScreen()
+		{
+			Lcd.Instance.DrawEllipseFilled (_center, 60, 30, true);
+			Lcd.Instance.DrawEllipseFilled (new Point(0, Lcd.Height/2) , 60, 30, false);
+
+			var r = new Rectangle (new Point (15, 28), new Point (Lcd.Width - 15, Lcd.Height - 25));
+			Lcd.Instance.DrawRectangle (r, true);
+		}
+
+	}
+}
+
---
+++ LcdDrawExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MonoBrickFirmware.Display.Dialogs;
+
+namespace LcdDraw
+{
+	public class Program
+	{
+		public static void Main (string[] args)
+		{
+			var examplesDialog = new ExamplesDialog ("Draw example");
+			examplesDialog.Show ();
+		}
+	}
+}
