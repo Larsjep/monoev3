0164f01b30960e495a077da24eabb50b8798f491
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2014-11-27 20:43:53 +0100

    Added documentation model to web server

---
+++ MonoBrickWebServer/Models/DokumentationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MonoBrickWebServer
+{
+	public class DokumentationModel
+	{
+		public DokumentationModel (string url, string description)
+		{
+			URL = url;
+			Description = description;
+		}
+		public string URL { get; private set; }
+		public string Description { get; private set; }
+	}
+}
+
