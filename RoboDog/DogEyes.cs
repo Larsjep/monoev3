d500b27a328700be0f8a897a92517e17e1287242
Stochholm <simo@tietgen.dk>
2014-12-04 13:59:08 +0100

    Create DogEyes.cs

---
+++ RoboDog/DogEyes.cs
@@ -0,0 +1,38 @@
+namespace RoboDog
+{
+    /// <summary>
+    /// Eyes
+    /// </summary>
+    public enum DogEyes
+    {
+        Angry,
+        Awake,
+        BlackEye,
+        BottomLeft,
+        Crazy1,
+        Crazy2,
+        Disappointed,
+        Dizzy,
+        Down,
+        Evil,
+        Hurt,
+        KnockedOut,
+        Love,
+        MiddleLeft,
+        MiddleRight,
+        Neutral,
+        Nuclear,
+        PinchLeft,
+        PinchMiddle,
+        PinchRight,
+        Sleeping,
+        Tear,
+        TiredLeft,
+        TiredMiddle,
+        TiredRight,
+        Toxic,
+        Up,
+        Winking
+        
+    }
+}
