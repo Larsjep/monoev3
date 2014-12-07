f23253ae758f0a92ced6c3e803c19a8a1d7335d3
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2014-12-07 23:58:57 +0100

    Updated EV3 web server example

--- MonoBrickWebserverEV3Test/Program.cs
+++ MonoBrickWebserverEV3Test/Program.cs
@@ -19,32 +19,6 @@ namespace MonoBrickWebserverEV3Test
 			return Webserver.Instance.Start(8080, 3 * 60000);
 		}
 
-		static bool LoadPage (string url)
-		{
-			int attemps = 0;
-			bool loaded = false;
-			while(attemps < 2 && !loaded){
-				try 
-				{
-					HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
-					myRequest.Method = "GET";
-					WebResponse myResponse = myRequest.GetResponse();
-					StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
-					string result = sr.ReadToEnd();
-					Console.WriteLine(result);
-					sr.Close();
-					myResponse.Close();
-					loaded = true;
-				} 
-				catch(Exception e) 
-				{
-					Console.WriteLine(e.StackTrace);
-				}
-				attemps++;
-			}
-			return loaded;
-		}
-
 		static void StopWebserver ()
 		{
 			Webserver.Instance.Stop();
@@ -52,14 +26,8 @@ namespace MonoBrickWebserverEV3Test
 
 		static void Main (string[] args)
 		{
-			List<IStep> steps = new List<IStep> ();
-			Console.WriteLine ("Adding tasks to list");
-			steps.Add (new StepContainer (StartWebserver, "Starting server", "Failed to start"));
-			steps.Add (new StepContainer (delegate() 
-			{
-				return LoadPage ("http://127.0.0.1:8080/");
-			}, "Loading pages", "Load failed"));
-			Dialog dialog = new StepDialog ("Webserver", steps);
+			var step = new StepContainer (StartWebserver, "Starting server", "Failed to start");
+			Dialog dialog = new ProgressDialog("Webserver", step);
 			if (dialog.Show ()) {
 				dialog = new InfoDialog ("Press enter to stop", true, "Webserver");
 				dialog.Show ();
