f1d8d31aa52449922d2ff9bfeb0bfd23bd1fddca
AndersSoborg <anderssoborg@Anderss-MacBook-Pro.local>
2014-12-17 21:54:05 +0100

    Added sensorattachment class with wait for sensor support
    Split existing classes into separate files

---
+++ MonoBrickFirmware/Sensors/SensorAttachment.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+
+namespace MonoBrickFirmware.Sensors
+{
+	public class SensorAttachment
+	{
+		private static int PollInterval = 500;
+		/// <summary>
+		/// Wait for a sensor on a specified port.
+		/// </summary>
+		/// <param name="port">Port the sensor should be acctached to</param>
+		/// <typeparam name="SensorType">The sensor type to wait for</typeparam>
+		public static SensorType Wait<SensorType> (SensorPort port)
+			where SensorType : ISensor
+		{
+			ManualResetEvent sensorAttached = new ManualResetEvent(false);
+			SensorDetector detector = new SensorDetector();
+			bool run = true;
+			SensorType newSensor = default(SensorType);
+			detector.SensorAttached += delegate(ISensor sensor) 
+			{
+				if(sensor.Port == port && sensor.GetType() == typeof(SensorType))
+				{
+					newSensor = (SensorType)sensor;
+					sensorAttached.Set();
+					run = false;
+				}		
+			};
+			while (run) 
+			{
+		    	detector.Update();
+				sensorAttached.WaitOne(PollInterval);
+			}
+			return newSensor;
+		}
+
+		/// <summary>
+		/// Wait for a sensor on any port.
+		/// </summary>
+		/// <typeparam name="SensorType">The sensor type to wait for</typeparam>
+		public static SensorType Wait<SensorType>()
+			where SensorType : ISensor
+		{
+			ManualResetEvent sensorAttached = new ManualResetEvent(false);
+			SensorDetector detector = new SensorDetector();
+			bool run = true;
+			SensorType newSensor = default(SensorType);
+			detector.SensorAttached += delegate(ISensor sensor) 
+			{
+				if(sensor.GetType() == typeof(SensorType))
+				{
+					sensorAttached.Set();
+					newSensor = (SensorType)sensor;
+					run = false;
+				}		
+			};
+			while (run) 
+			{
+		    	detector.Update();
+				sensorAttached.WaitOne(PollInterval);
+			}
+			sensorAttached.WaitOne();
+			return newSensor;
+		}
+
+
+	}
+}
+
--- MonoBrickFirmware/Sensors/SensorListner.cs
+++ MonoBrickFirmware/Sensors/SensorListner.cs
@@ -3,60 +3,6 @@ using System.Threading;
 using MonoBrickFirmware.Tools;
 namespace MonoBrickFirmware.Sensors
 {
-	public class SensorDetector
-	{
-		private SensorType[] lastSensorType = new SensorType[SensorManager.NumberOfSensorPorts];
-		private SensorPort[] sensorPort = { SensorPort.In1, SensorPort.In2, SensorPort.In3, SensorPort.In4 };
-		private ISensor[] sensor = new ISensor[SensorManager.NumberOfSensorPorts];
-		private bool hasBeenInitialized = false;
-		public event Action<ISensor> SensorAttached = delegate { };
-		public event Action<SensorPort> SensorDetached = delegate { };
-
-		public void Update()
-		{
-			if (!hasBeenInitialized)
-			{
-				Initialize();
-				hasBeenInitialized = true;
-			}
-			for (int i = 0; i < SensorManager.NumberOfSensorPorts; i++)
-			{
-				SensorType currentType = SensorManager.Instance.GetSensorType(sensorPort[i]);
-				if (currentType != lastSensorType[i])
-				{
-					sensor[i] = SensorFactory.GetSensor(sensorPort[i]);
-					lastSensorType[i] = currentType;
-					if (currentType == SensorType.None)
-					{
-						ResetSensor(sensorPort[i]);
-						SensorDetached(sensorPort[i]);
-					}
-					else
-					{
-						SensorAttached(sensor[i]);
-					}
-				}
-			}
-		}
-
-		private void ResetSensor(SensorPort port)
-		{
-			SensorManager.Instance.ResetUart(port);
-			SensorManager.Instance.ResetI2C(port);
-			SensorManager.Instance.SetAnalogMode(AnalogMode.None, port);
-		}
-
-		private void Initialize()
-		{
-			for (int i = 0; i < SensorManager.NumberOfSensorPorts; i++)
-			{
-				lastSensorType[i] = SensorType.None;
-				ResetSensor(sensorPort[i]);
-				sensor[i] = null;
-			}
-		}
-	}
-  
 	public class SensorListner : IDisposable
 	{
 		private SensorDetector detector = new SensorDetector();
