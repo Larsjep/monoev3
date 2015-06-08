2ce65f9fa6b9329e1a331c9ae44424543588daea
Anders Søborg <anders@soborg.net>
2015-06-08 07:31:27 +0200

    Added new menu items

---
+++ MonoBrickFirmware/Display/Menus/ItemWithCheckBoxStep.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using MonoBrickFirmware.Display.Dialogs;
+
+namespace MonoBrickFirmware.Display.Menus
+{
+	public class ItemWithCheckBoxStep : ItemWithCheckBox, IParentItem
+	{
+		private CheckBoxStep itemStep = null;
+		private string stepTitle;
+		private ItemWithProgressDialog dialogItem;
+		public ItemWithCheckBoxStep (string text, bool checkedAtStart, string stepTitle, CheckBoxStep step) : base(text, checkedAtStart)
+		{
+			this.stepTitle = stepTitle;
+			this.itemStep = step;
+			dialogItem = new ItemWithProgressDialog (new ProgressDialog(stepTitle, itemStep));
+		}
+		public void OnEnterPressed ()
+		{
+			itemStep.Checked = this.Checked;
+			dialogItem.SetFocus(this);	
+		}
+
+		public void OnHideContent ()
+		{
+			dialogItem.OnHideContent ();
+		}
+
+		public void OnUpPressed ()
+		{
+
+		}
+
+		public void OnDownPressed ()
+		{
+
+		}
+
+		public void OnEscPressed ()
+		{
+
+		}
+
+		#region IParentItem implementation
+
+		public void SetFocus (IChildItem item)
+		{
+			Parent.SetFocus (item);
+		}
+
+		public virtual void RemoveFocus (IChildItem item)
+		{
+			Parent.SetFocus(item);
+		}
+
+		public void SuspendEvents (IChildItem item)
+		{
+			Parent.SuspendEvents (item);
+		}
+
+		public void ResumeEvents (IChildItem item)
+		{
+			Parent.SuspendEvents (item);
+		}
+
+		#endregion
+
+	}
+
+	public class CheckBoxStep: IStep
+	{
+		private IStep checkedStep;
+		private IStep unCheckStep;
+
+		public CheckBoxStep(IStep checkedStep, IStep unCheckedStep)
+		{
+			this.checkedStep = checkedStep;
+			this.unCheckStep = unCheckedStep;
+		}
+
+
+		public bool Checked{get; set;}
+		public string StepText{get {return Checked ? checkedStep.StepText : unCheckStep.StepText;}}
+		public string ErrorText{get {return Checked ? checkedStep.ErrorText : unCheckStep.ErrorText;}}
+		public string OkText{get {return Checked ? checkedStep.OkText : unCheckStep.OkText;}}
+		public bool ShowOkText{get {return Checked ? checkedStep.ShowOkText : unCheckStep.ShowOkText;}}
+		public bool Execute ()
+		{
+			return Checked ? checkedStep.Execute () : unCheckStep.Execute ();
+		}
+	}
+
+	internal class ItemWithProgressDialog : ItemWithDialog<ProgressDialog>
+	{
+		public ItemWithProgressDialog(ProgressDialog dialog): base(dialog)
+		{
+
+		}
+
+		public override void OnExit (ProgressDialog dialog)
+		{
+			((ItemWithCheckBox)Parent).Checked = dialog.Ok;
+			Parent.RemoveFocus (this);
+		}
+	}
+}
+
---
+++ MonoBrickFirmware/Display/Menus/ItemWithDialog.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using MonoBrickFirmware.Display.Dialogs;
+
+namespace MonoBrickFirmware.Display.Menus
+{
+	public abstract class ItemWithDialog<DialogType> : IChildItem
+		where DialogType : Dialog
+	{
+
+		private string title;
+		private bool hasFocus = false;
+		protected DialogType dialog;
+
+		public abstract void OnExit (DialogType dialog);
+
+		public ItemWithDialog(DialogType dialog):this(dialog, "")
+		{
+		
+		}
+
+
+		public ItemWithDialog(DialogType dialog, String title)
+		{
+			this.title = title;
+			this.dialog = dialog;
+			dialog.OnExit += OnDone;
+		}
+
+		public IParentItem Parent { get; set;}
+
+
+		/// <summary>
+		/// Set focus from this parent menu item
+		/// </summary>
+		/// <param name="menuItem">Menu item.</param>
+		public void SetFocus(IParentItem menuItem)
+		{
+			this.Parent = menuItem;
+			hasFocus = true;
+			Parent.SetFocus (this);
+		}
+
+
+		public void OnEnterPressed ()
+		{
+			if (hasFocus) 
+			{
+				dialog.OnEnterPressed();	
+			} 
+			else 
+			{
+				hasFocus = true;
+				Parent.SetFocus (this);
+			}
+
+		}
+
+		public void OnLeftPressed ()
+		{
+			if (hasFocus) 
+			{
+				dialog.OnLeftPressed ();
+			}
+		}
+
+		public void OnRightPressed ()
+		{
+			if (hasFocus) 
+			{
+				dialog.OnRightPressed ();
+			}
+		}
+
+		public void OnUpPressed ()
+		{
+			if (hasFocus) 
+			{
+				dialog.OnUpPressed ();
+			}
+		}
+
+		public void OnDownPressed ()
+		{
+			if (hasFocus) 
+			{
+				dialog.OnDownPressed ();
+			}
+		}
+		public void OnEscPressed ()
+		{
+			if (hasFocus) 
+			{
+				dialog.OnEscPressed ();
+			}
+		}
+
+		public void OnDrawTitle (Font font, Rectangle rectangle, bool selected)
+		{
+			Lcd.Instance.WriteTextBox (font, rectangle, title, selected);
+		}
+
+		public void OnDrawContent ()
+		{
+			if (hasFocus) 
+			{
+				dialog.Draw ();
+			}
+		}
+		public void OnHideContent ()
+		{
+			if (hasFocus)
+			{
+				dialog.Hide ();
+			}
+		}
+
+		private void OnDone()
+		{
+			Parent.RemoveFocus (this);
+			OnExit(this.dialog);
+		}
+
+	}
+}
+
