435821214e4dd4c18ca1cdc8644e3ab1fa85a77f
Anders Søborg <anders@soborg.net>
2015-06-10 07:12:54 +0200

    Added new shutdown menu item

---
+++ MonoBrickFirmware/Display/Menus/ItemWithShutDown.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MonoBrickFirmware.Display.Dialogs;
+using MonoBrickFirmware.UserInput;
+using MonoBrickFirmware.Native;
+using System.Threading;
+
+namespace MonoBrickFirmware.Display.Menus
+{
+	public class ItemWithShutDown : ItemWithDialog<QuestionDialog>
+	{
+		public ItemWithShutDown () : base (new QuestionDialog ("Are you sure?", "Shutdown EV3"), "Shutdown")
+		{
+			
+		}
+
+		public override void OnExit (QuestionDialog dialog)
+		{
+			if (dialog.IsPositiveSelected) 
+			{
+				Lcd.Instance.Clear();
+				Lcd.Instance.WriteText(Font.MediumFont, new Point(0,0), "Shutting down...", true);
+				Lcd.Instance.Update();
+
+				Buttons.Instance.LedPattern(2);
+				ProcessHelper.RunAndWaitForProcess("/sbin/shutdown", "-h now");
+				Thread.Sleep(120000);
+				Parent.RemoveFocus (this);
+			} 
+			else 
+			{
+				Parent.RemoveFocus (this);
+			}
+		}
+	}
+}
+
