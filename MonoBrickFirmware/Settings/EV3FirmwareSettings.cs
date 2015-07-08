71219ffa9d1d7e5b9f3bb9dfb5e5305ac1c0f5fd
Anders Søborg <anders@soborg.net>
2015-07-08 23:15:51 +0200

    Moved settings interface to operate class

---
+++ MonoBrickFirmware/Settings/EV3FirmwareSettings.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System;
+using System.Xml.Serialization;
+using System.IO;
+using MonoBrickFirmware.Tools;
+
+namespace MonoBrickFirmware.Settings
+{
+
+	[XmlRoot("ConfigRoot")]
+	public class EV3FirmwareSettings : IFirmwareSettings
+	{
+		private object readWriteLock = new object();
+		protected string SettingsFileName = "/mnt/bootpar/firmwareSettings.xml";
+		public EV3FirmwareSettings()
+		{
+			GeneralSettings = new GeneralSettings();
+			WiFiSettings = new WiFiSettings();
+			SoundSettings = new SoundSettings();
+			WebServerSettings = new WebServerSettings();
+		}
+
+		[XmlElement("GeneralSettings")]
+		public IGeneralSettings GeneralSettings { get ; set;}
+
+		[XmlElement("WiFiSettings")]
+		public IWiFiSettings WiFiSettings { get ; set;}
+
+		[XmlElement("SoundSettings")]
+		public ISoundSettings SoundSettings { get ; set;}
+
+		[XmlElement("WebServerSettings")]
+		public IWebServerSettings WebServerSettings { get ; set;}
+
+		public bool Save()
+		{
+			lock (readWriteLock) {
+				TextWriter textWriter = null;
+				try {
+					XmlSerializer serializer = XmlHelper.CreateSerializer(typeof(EV3FirmwareSettings));
+					textWriter = new StreamWriter (SettingsFileName);
+					serializer.Serialize (textWriter, this);
+					textWriter.Close ();
+					return true;
+				} 
+				catch (Exception exp) 
+				{ 
+					Console.WriteLine("Exception during settings save: " + exp.Message);
+					Console.WriteLine(exp.StackTrace);
+				}
+				if(textWriter!= null)
+					textWriter.Close();
+			}
+			return false;
+		}
+
+		public void Load()
+		{
+			lock (readWriteLock) 
+			{
+				TextReader textReader = null;
+				try{
+					XmlSerializer deserializer = XmlHelper.CreateSerializer(typeof(EV3FirmwareSettings));
+					textReader = new StreamReader (SettingsFileName);
+					Object obj = deserializer.Deserialize (textReader);
+					var loadSettings = (EV3FirmwareSettings)obj;
+					textReader.Close ();
+					GeneralSettings = loadSettings.GeneralSettings;
+					WiFiSettings = loadSettings.WiFiSettings;
+					SoundSettings = loadSettings.SoundSettings;
+					WebServerSettings = loadSettings.WebServerSettings;
+				}
+				catch (FileNotFoundException exp)
+				{
+					Save();
+				}
+				if(textReader!= null)
+					textReader.Close();
+			}
+		}
+	}
+
+	public class WebServerSettings : IWebServerSettings
+	{
+		[XmlElement("Port")]
+		private int port = 80;
+
+		public int Port
+		{
+			get { return port; }
+			set { port = value; }
+		}
+	}
+
+	public class WiFiSettings : IWiFiSettings
+	{
+		[XmlElement("SSID")]
+		private string ssid = "YourSSID";
+
+		[XmlElement("Password")]
+		private string password = "YourPassword";
+
+		[XmlElement("Encryption")]
+		private bool encryption = true;
+
+		public string SSID {
+			get{return ssid; }
+			set { ssid = value; }
+		}
+		public string Password{
+			get{return  password; }
+			set {  password = value; }
+		}
+
+		public bool Encryption	
+		{
+			get { return encryption; }
+			set { encryption = value; }
+		}
+	}
+
+	public class GeneralSettings : IGeneralSettings
+	{
+		[XmlElement("CheckForSwUpdatesAtStartUp")]
+		private bool checkForSwUpdatesAtStartUp = false;
+
+		[XmlElement("ConnectToWiFiAtStartUp")]
+		private bool connectToWiFiAtStartUp = false;
+
+		public bool CheckForSwUpdatesAtStartUp	
+		{
+			get { return checkForSwUpdatesAtStartUp; }
+			set { checkForSwUpdatesAtStartUp = value; }
+		}
+
+		public bool ConnectToWiFiAtStartUp	
+		{
+			get { return connectToWiFiAtStartUp; }
+			set { connectToWiFiAtStartUp = value; }
+		}
+	}
+
+	public class SoundSettings: ISoundSettings
+	{
+		[XmlElement("Volume")]
+		private int volume = 60;
+
+		[XmlElement("EnableSound")]
+		private bool enableSound = true;
+
+		public bool EnableSound	
+		{
+			get { return enableSound; }
+			set { enableSound = value; }
+		}
+
+
+		public int Volume	
+		{
+			get { return volume; }
+			set { volume = value; }
+		}
+	}
+
+}
+
---
+++ MonoBrickFirmware/Settings/IFirmwareSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace MonoBrickFirmware.Settings
+{
+
+	public interface IFirmwareSettings
+	{
+		IGeneralSettings GeneralSettings { get; set; }
+		IWiFiSettings WiFiSettings { get; set; }
+		ISoundSettings SoundSettings{ get; set; }
+		IWebServerSettings WebServerSettings{ get; set; }
+		bool Save ();
+		void Load();
+	}
+
+	public class IWebServerSettings
+	{
+		public int Port{ get; set;}
+	}
+
+	public class IWiFiSettings
+	{
+		public string SSID { get; set;}
+		public string Password{ get; set;}
+		public bool Encryption{ get; set;}
+	}
+
+	public class IGeneralSettings{
+		public bool CheckForSwUpdatesAtStartUp	{ get; set;}
+
+		public bool ConnectToWiFiAtStartUp{ get; set;}
+	}
+
+	public class ISoundSettings
+	{
+		public bool EnableSound{ get; set;}
+		public int Volume{ get; set;}
+	}
+
+}
+
