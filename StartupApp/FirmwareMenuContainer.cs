22d7bd915d9aa68386e5f11b631a329c5512e7d0
Anders Søborg <anders@soborg.net>
2015-07-19 21:00:07 +0200

    Fixed bug with checking for updates not working at startup

--- StartupApp/FirmwareMenuContainer.cs
+++ StartupApp/FirmwareMenuContainer.cs
@@ -11,11 +11,16 @@ namespace StartupApp
 
 		public void Show(int startIndex)
 		{
+			topMenu.OnDrawContent ();
 			for (int i = 0; i < startIndex; i++) 
 			{
 				topMenu.OnDownPressed ();		
 			}
 			topMenu.OnEnterPressed ();
+			for (int i = 0; i < startIndex; i++) 
+			{
+				topMenu.OnUpPressed ();	
+			}
 			Show ();
 		}
 	}
