f54dbea490dee1d9197238777f496b206d77f5ff
Anders Søborg <anders@soborg.net>
2015-08-26 23:52:09 +0200

    Updated menu example to comply with new interface

--- MenuExample/Program.cs
+++ MenuExample/Program.cs
@@ -22,16 +22,14 @@ namespace MenuExample
 			IStep turnOnStep = new StepContainer (TurnOn, "Turning On", "Error turning on", "Turned on sucesfully");
 			IStep turnOffStep = new StepContainer (TurnOff, "Turning Off", "Error turning off");
 
-
-			subSubMenu.AddItem( new ItemWithCheckBoxStep("Turned on", false, "Device", new CheckBoxStep(turnOnStep, turnOffStep ));
-			subSubMenu.AddItem( new ItemWithNumericInput("Age", 29, 0, 100));
-			subSubMenu.AddItem( new ItemWithOptions<string>("Option", new string[]{"Male","Female"}));
-			subSubMenu.AddItem( new ItemWithCheckBox("Loves C#", true));
+			subSubMenu.AddItem( new ItemWithCheckBoxStep("Turned on", false, "Device", new CheckBoxStep(turnOnStep, turnOffStep), OnTurnOnChanged  ));
+			subSubMenu.AddItem (new MonoBrickQuestion());
+			subSubMenu.AddItem( new ItemWithBrickInfo());
 
 			subMenu.AddItem (subSubMenu);
-			subMenu.AddItem( new ItemWithCharacterInput("Name", "Enter Name", "Anders"));
-			subMenu.AddItem( new ItemWithNumericInput("Age", 29, 0, 100));
-			subMenu.AddItem( new ItemWithCheckBox("Loves C#", true));
+			subMenu.AddItem( new ItemWithCharacterInput("Name", "Enter Name", "Anders", OnNameChanged));
+			subMenu.AddItem( new ItemWithNumericInput("Age", 29, OnAgeChanged, 0, 100));
+			subMenu.AddItem( new ItemWithCheckBox("Loves C#", true, OnCheckedChanged));
 
 			mainMenu.AddItem (subMenu);
 			mainMenu.AddItem (new ItemWithProgramList ("Programs"));
@@ -39,6 +37,26 @@ namespace MenuExample
 			container = new MenuContainer(mainMenu);
 			container.Show();
 			
+		}
+
+		private static void OnTurnOnChanged(bool isOn)
+		{
+			Console.WriteLine ("Turn on set to  " + isOn);	
+		}
+
+		private static void OnCheckedChanged(bool isChecked)
+		{
+			Console.WriteLine ("Loves C# is set to " + isChecked);
+		}
+
+		private static void OnNameChanged(string newName)
+		{
+			Console.WriteLine ("Name was changed to" + newName);
+		}
+
+		private static void OnAgeChanged(int newAge)
+		{
+			Console.WriteLine ("Your new age is " + newAge);
 		}
 		
 		private static bool TurnOn()
@@ -52,6 +70,25 @@ namespace MenuExample
 			System.Threading.Thread.Sleep (500);
 			return true; //return false if it fails
 		}
+
+		private class MonoBrickQuestion : ItemWithDialog<QuestionDialog>
+		{
+			public MonoBrickQuestion() : base( new QuestionDialog ("Do you Like MonoBrick?", "MonoBrick"))
+			{
+				
+			}
+
+			public override void OnExit (QuestionDialog dialog)
+			{
+				Console.WriteLine ("Do you like MonoBrick is set to " + dialog.IsPositiveSelected);
+			}
+
+		}
+
+
+
+
+
 		
 
 	}
