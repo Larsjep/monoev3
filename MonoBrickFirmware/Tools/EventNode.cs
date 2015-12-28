2f1b7d4d2f6a403789f47404df8ba04b62528324
Lars Gardien <larsgardien@live.nl>
2015-12-28 17:10:32 +0100

    Added the EventNode, forgot it the previous commit

---
+++ MonoBrickFirmware/Tools/EventNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoBrickFirmware.Tools
+{
+    public class EventNode
+    {
+        private Delegate EventToRaise;
+        private Object[] Parameters;
+
+        public EventNode(Delegate Event, Object[] Parameters)
+        {
+            this.EventToRaise = Event;
+            this.Parameters = Parameters;
+        }
+
+        public Delegate eventToRaise {
+            get { return this.EventToRaise; }
+        }
+        public Object[] parameters {
+            get { return this.Parameters; }
+        }
+    }
+}
